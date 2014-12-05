2cbc0bab016b31ca6b88abe9fee2cea94b908562
Trent Mick <trentm@gmail.com>
2014-12-04 16:27:34 -0800

    Allow repo:tag arg to this example

--- examples/session.getImgAncestry.js
+++ examples/session.getImgAncestry.js
@@ -12,15 +12,21 @@
 
 var docker = require('../');
 
-var repo = 'library/mongo';
-docker.createRegistrySession({repo: repo}, function (err, sess) {
+// Default to "library/mongo:latest".
+var rat = docker.parseRepoAndTag(
+        process.argv.length > 2 ? process.argv[2] : 'mongo');
+
+docker.createRegistrySession({repo: rat.repo}, function (err, sess) {
     if (err) {
         console.error(err.message);
         return;
     }
     sess.listRepoTags(function (listErr, repoTags) {
-        var latest = repoTags['latest'];
-        sess.getImgAncestry({imgId: latest}, function (getErr, ancestry) {
+        // The `|| rat.tag` is a hack to allow passing in a specific untagged
+        // imgId.
+        var imgId = repoTags[rat.tag] || rat.tag;
+
+        sess.getImgAncestry({imgId: imgId}, function (getErr, ancestry) {
             if (getErr) {
                 console.error(getErr.message);
                 return;
